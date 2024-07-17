849667f3b32a65c123b14f03d5d03b60133d1e36
James Brotsos <j.brotsos@gmail.com>
2024-07-17 15:00:58 -0700

    chore: Remove unused code and comments in Dockerfile and xss_blank5.java

--- src/xss_blank5.java
+++ src/xss_blank5.java
@@ -3,15 +3,4 @@ class xss_blank5 {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
     }
-
-    // Method to display search term from a query parameter
-    public static void displaySearchTerm(String searchTerm) {
-        // Simulate displaying the search term on a resulting page
-        System.out.println("Search Term: " + sanitizeInput(searchTerm));
-    }
-
-    // Method to sanitize input to prevent XSS attacks
-    private static String sanitizeInput(String input) {
-        return input.replaceAll("<", "&lt;").replaceAll(">", "&gt;");
-    }
 }
