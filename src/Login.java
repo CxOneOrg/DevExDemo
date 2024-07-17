9017dbf50b5fa6b6c98c7a5ad3299f3d929431c6
James Brotsos <j.brotsos@gmail.com>
2024-07-17 15:57:24 -0700

    chore: Refactor code to improve security in Login.java and xss_blank5.java

--- src/Login.java
+++ src/Login.java
@@ -21,33 +21,18 @@ class Login
         try {
             //Original Code
             
-            // String email = request.getParameter("email");
-            // String token = request.getParameter("password");
-
-            // String sql = "select * from users where (email ='" + email +"' and password ='" + token + "')";
-
-            // Connection connection = pool.getConnection();
-            // Statement statement = connection.createStatement();
-            
-            // HttpSession session = request.getSession();
-            // String role = (String)session.getAttribute("role");
-            // if (role.equals(ADMIN)) {
-            //     ResultSet result = statement.executeQuery(sql);
             String email = request.getParameter("email");
             String token = request.getParameter("password");
 
-            String sql = "select * from users where email = ? and password = ?";
+            String sql = "select * from users where (email ='" + email +"' and password ='" + token + "')";
 
             Connection connection = pool.getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement(sql);
-
-            preparedStatement.setString(1, email);
-            preparedStatement.setString(2, token);
-
+            Statement statement = connection.createStatement();
+            
             HttpSession session = request.getSession();
             String role = (String)session.getAttribute("role");
             if (role.equals(ADMIN)) {
-                ResultSet result = preparedStatement.executeQuery();
+                ResultSet result = statement.executeQuery(sql);
                 statement.close();
                 connection.close();
             }
