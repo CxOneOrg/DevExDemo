95321f12c1df4eb1c659e6396b4390dd116184ff
James Brotsos <j.brotsos@gmail.com>
2024-08-07 08:02:40 -0700

    chore: Refactor code to improve security in Login.java and xss_blank5.java

--- src/Login.java
+++ src/Login.java
@@ -21,20 +21,36 @@ class Login
         try {
             //Original Code
             
+            // String email = request.getParameter("email");
+            // String token = request.getParameter("password");
+
+            // String sql = "select * from users where (email ='" + email +"' and password ='" + token + "')";
+
+            // Connection connection = pool.getConnection();
+            // Statement statement = connection.createStatement();
+            
+            // HttpSession session = request.getSession();
+            // String role = (String)session.getAttribute("role");
+            // if (role.equals(ADMIN)) {
+            //     ResultSet result = statement.executeQuery(sql);
+            //     statement.close();
+            //     connection.close();
+            // }
+
             String email = request.getParameter("email");
             String token = request.getParameter("password");
-
-            String sql = "select * from users where (email ='" + email +"' and password ='" + token + "')";
-
+    
+            String sql = "select * from users where email = ? and password = ?";
+            
             Connection connection = pool.getConnection();
-            Statement statement = connection.createStatement();
+            PreparedStatement preparedStmt = connection.prepareStatement(sql);
+            preparedStmt.setString (1, email);
+            preparedStmt.setString (2, token);
             
             HttpSession session = request.getSession();
             String role = (String)session.getAttribute("role");
             if (role.equals(ADMIN)) {
-                ResultSet result = statement.executeQuery(sql);
-                statement.close();
-                connection.close();
+                ResultSet result = preparedStmt.executeQuery();
             }
             
         catch (SQLException ex) {
