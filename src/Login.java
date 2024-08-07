7aa6ebdcb9260796afda0c67bb7c3d00ffa98ec6
James Brotsos <j.brotsos@gmail.com>
2024-08-07 10:17:29 -0700

    chore: Update Dockerfile to set user and working directory

--- src/Login.java
+++ src/Login.java
@@ -19,24 +19,41 @@ class Login
     public static void main(String[] args)   
     {
         try {
-            Original Code
+            // Original Code
             
-            String email = request.getParameter("email");
-            String token = request.getParameter("password");
+            // String email = request.getParameter("email");
+            // String token = request.getParameter("password");
 
-            String sql = "select * from users where (email ='" + email +"' and password ='" + token + "')";
+            // String sql = "select * from users where (email ='" + email +"' and password ='" + token + "')";
 
-            Connection connection = pool.getConnection();
-            Statement statement = connection.createStatement();
+            // Connection connection = pool.getConnection();
+            // Statement statement = connection.createStatement();
             
-            HttpSession session = request.getSession();
-            String role = (String)session.getAttribute("role");
-            if (role.equals(ADMIN)) {
-                ResultSet result = statement.executeQuery(sql);
-                statement.close();
-                connection.close();
-            }
-            
+            // HttpSession session = request.getSession();
+            // String role = (String)session.getAttribute("role");
+            // if (role.equals(ADMIN)) {
+            //     ResultSet result = statement.executeQuery(sql);
+            //     statement.close();
+            //     connection.close();
+            // }
+            String email = request.getParameter("email");//SAST Node #0 (input): &#34;&#34;email&#34;&#34; ()//SAST Node #1: getParameter ()//SAST Node #2: email ()
+        String token = request.getParameter("password");
+
+        String sql = "select * from users where email = ? and password = ?"; //SAST Node #3: email ()//SAST Node #4: sql ()
+
+        Connection connection = pool.getConnection();
+        PreparedStatement statement = connection.prepareStatement(sql);
+
+        statement.setString(1, email);
+        statement.setString(2, token);
+
+        HttpSession session = request.getSession();
+        String role = (String)session.getAttribute("role");
+
+        if (role.equals(ADMIN)){
+            ResultSet result = statement.executeQuery();//SAST Node #5: sql ()//SAST Node #6 (output): executeQuery ()
+        }
+        
         catch (SQLException ex) {
             handleExceptions(ex);
         }
